f9202433965cb6332c2479edc513e6ba5661e646
John <91637405+MrXez@users.noreply.github.com>
2021-10-19 19:45:44 +0100

    Update GuildMemberRemove.js

--- logging/GuildMemberRemove.js
+++ logging/GuildMemberRemove.js
@@ -3,14 +3,14 @@ const { channelLog } = require('../config/constants/channel.json');
 
 module.exports = (client) => {
   client.on('guildMemberRemove', async (member) => {
-    const logs = await client.channels.cache.get(channelLog);
+    const logs = await client.channel.cache.get(channelLog);
     const embed = new discord.MessageEmbed()
       .setTitle('Member Left')
       .setColor('GREEN')
       .setDescription('A member left the server.')
       .addField('User', member.user.tag, true)
       .addField('User ID', member.id, true)
-      .addField('User Account Registered At', member.user.createdAt)
+      .addField('User Account Registered At', member.user.createdAt.toString())
       .setThumbnail(member.user.displayAvatarURL({ dynamic: true }));
     return logs.send({ embeds: [embed] });
   });
