4d637183b7987f42f578b35d25c7dd302b9323dc
XEZ123 <discordraspberrypi@gmail.com>
2021-10-20 18:07:49 +0100

    Update verification.js
    made a few changes

--- events/verification.js
+++ events/verification.js
@@ -37,7 +37,7 @@ module.exports = {
           return;
         }
         try {
-          channel.send({embeds: [e1.setImage(captchaImage)]}
+          channel.send({ embeds: [e1.setImage(captchaImage)] }
           ).catch(async () => {
             vchannel = client.channels.cache.get(verificationchannel);
             const enableDMEmb = new Discord.MessageEmbed()
@@ -45,7 +45,7 @@ module.exports = {
               .setDescription(`please enable DMs then run the command ${prefix}verify`)
               .setImage('https://i.imgur.com/sEkQOCf.png');
 
-            vchannel.send({ content: `<@!${args[0].user.id}>`, embed: enableDMEmb }).then((message) => message.delete({ timeout: 20000 }));
+            vchannel.send({ content: `<@!${args[0].user.id}>`, embeds: enableDMEmb }).then((message) => message.delete({ timeout: 20000 }));
           });
         } catch (err) {
           console.log(err);
@@ -58,7 +58,7 @@ module.exports = {
             return true;
           }
           console.log(`incorrect captcha: ${userCaptchaData[args[0].id].captchaValue} // got : ${String(m.content).toUpperCase()}`);
-          m.channel.send(e2); // captcha is incorrect and messages the user it is incorrect
+          m.channel.send({ embeds: [e2] }); // captcha is incorrect and messages the user it is incorrect
           return false;
         };
         channel.awaitMessages(filter, {
@@ -73,37 +73,13 @@ module.exports = {
               vchannel = client.channels.cache.get(verificationchannel);
               var roleObj = args[0].guild.roles.cache.get(roleID);
               if (roleObj) {
-                await channel.send(e3);
+                await channel.send({embeds: [e3]});
                 await args[0].roles.add(roleObj);
               }
             }
             // if the new member joins and enters captcha code correctly, the log will go to the specific channel set by the server owner
             const joinedServer = args[0].guild.members.cache.get(args[0].user.id).joinedAt.toDateString();
             const userCreationDate = args[0].user.createdAt.toDateString();
-            const canvas = Canvas.createCanvas(700, 250);
-            const ctx = canvas.getContext('2d');
-            const background = await Canvas.loadImage(
-              path.join('./images/background.png'),
-            );
-            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-            ctx.strokeStyle = '#74037b';
-            ctx.strokeRect(0, 0, canvas.width, canvas.height);
-            // Slightly smaller text placed above the member's display name
-            ctx.font = '28px sans-serif';
-            ctx.fillStyle = '#ffffff';
-            ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-            // Add an exclamation point here and below
-            ctx.font = applyText(canvas, `${args[0].displayName}!`);
-            ctx.fillStyle = '#ffffff';
-            ctx.fillText(`${args[0].displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-            ctx.beginPath();
-            ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.clip();
-            const avatar = await Canvas.loadImage(args[0].user.displayAvatarURL({ format: 'jpg' }));
-            ctx.drawImage(avatar, 25, 25, 200, 200);
-            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-            welcomechannel123 = client.channels.cache.get(welcomeleave);
             const JoinedServer = args[0].guild.members.cache.get(args[0].user.id).joinedAt.toDateString();
             const userCreationDate1 = args[0].user.createdAt.toDateString();
             var roleObj = args[0].guild.roles.cache.get(roleID);
@@ -114,9 +90,8 @@ module.exports = {
               .addField('**Account Creation:**', `${userCreationDate}`)
               .addField('**Captcha Code:**', `${userCaptchaData[args[0].id].captchaValue}`)
               .addField('**Role Given:**', `${roleObj}`)
-              .setColor(Color);
-            if (channelLog) args[0].guild.channels.cache.get(channelLog).send(CaptchaLog);
-            welcomechannel123.send(`<@&826137014529032273>, Welcome ${member} to the server!`, attachment);
+              .setColor("Purple");
+            if (channelLog) args[0].guild.channels.cache.get(channelLog).send({ embeds: [CaptchaLog] });
           } catch (err) {
             console.log(err);
           }
