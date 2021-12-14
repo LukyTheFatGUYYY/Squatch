eac94e7abe1c0a75c277a87690c51b2a53f1f223
John Smith <91637405+MrXez@users.noreply.github.com>
2021-12-14 18:22:32 +0000

    fixed the channel logging

--- logging/ChannelCreate.js
+++ logging/ChannelCreate.js
@@ -5,12 +5,12 @@ module.exports = (client) => {
   client.on('channelCreate', async (channel) => {
     const logs = await client.channels.cache.get(channelLog);
     if (channel.type === 'GUILD_TEXT') {
-      const embed = new MessageEmbed()
+      const embed = new discord.MessageEmbed()
         .setTitle('Channel Created')
         .setColor('GREEN')
         .setDescription('A new channel was created.')
-        .addField('Channel', channel, true)
-        .addField('Channel ID', channel.id, true);
+        .addField('Channel', channel.toString(), true)
+        .addField('Channel ID', channel.id.toString(), true);
       return logs.send({ embeds: [embed] });
     }
   });
--- logging/ChannelDelete.js
+++ logging/ChannelDelete.js
@@ -5,16 +5,16 @@ module.exports = (client) => {
   client.on('channelDelete', async (channel) => {
     const logs = await client.channels.cache.get(channelLog);
     if (channel.type === 'GUILD_TEXT') {
-      const embed = new MessageEmbed()
+      const embed = new discord.MessageEmbed()
         .setTitle('Channel Deleted')
         .setColor('GREEN')
         .setDescription('A channel was deleted.')
         .addField('Channel Name', channel.name, true)
-        .addField('Channel ID', channel.id, true);
+        .addField('Channel ID', channel.id.toString(), true);
       if (channel.topic) {
         embed.addField('Channel Topic', channel.topic);
       }
-      embed.addField('NSFW', channel.nsfw);
+      embed.addField('NSFW', channel.nsfw.toString());
       return logs.send({ embeds: [embed] });
     }
   });
