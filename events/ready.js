c729638901da714ce5f11b2c8c0e70c33e457709
John Smith <91637405+MrXez@users.noreply.github.com>
2021-12-19 21:49:54 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,4 @@
 const eventLogger = require('../logging');
-
-const Discord = ('discord.js');
-// const client = new Discord.Client({
-//   intents: 4610, // guild memebers, guild messages, and DM intents
-// })
-// We don't want to make a new client, so I
-// just added a param to the event loader
-
 module.exports = {
   name: 'ready',
   runOnce: true,
