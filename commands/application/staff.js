a243c313bf86a4f8c0a3ae7752d461d0b412e864
John Smith <91637405+MrXez@users.noreply.github.com>
2021-12-22 15:44:47 +0000

    Update staff.js
    used buttons instead of select menu

--- commands/application/staff.js
+++ commands/application/staff.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const {
   staffapplicationChannel,
 } = require('../../config/constants/channel.json');
-const { staffmanagerrole } = require('../../config/constants/roles.json');
+const {
+  staffmanagerrole
+} = require('../../config/constants/roles.json');
 
 module.exports = {
   name: 'staff',
@@ -17,7 +19,9 @@ module.exports = {
         .setTitle('Application')
         .setDescription("Please check your DM's for the application")
         .setColor('GREEN');
-      message.channel.send({ embeds: [Application] });
+      message.channel.send({
+        embeds: [Application]
+      });
     }
     const filter = (m) => m.author.id === message.author.id;
     const cancel = new Discord.MessageEmbed()
@@ -28,11 +32,11 @@ module.exports = {
       .setTitle('Success')
       .setDescription("You have successfully submitted your application\nIf you don't recieve a response then most likely you have been denied")
       .setColor('GREEN');
-    const tomanychars  = new Discord.MessageEmbed()
+    const tomanychars = new Discord.MessageEmbed()
       .setTitle('Error')
       .setDescription("Too many characters")
       .setColor('RED');
-    const outoftime  = new Discord.MessageEmbed()
+    const outoftime = new Discord.MessageEmbed()
       .setTitle('Error')
       .setDescription("Out of time")
       .setColor('RED');
@@ -67,82 +71,81 @@ module.exports = {
     responses = [];
     let exitFlag = false;
     const skip = responses.length > 0;
-    const { author } = message;
+    const {
+      author
+    } = message;
     for (i = 0; i < questions.length; i++) {
       if (skip) break;
       questionEmbed.setTitle(`Question ${i + 1}`);
       questionEmbed.setDescription(questions[i]);
-      const m = await author.send({ embeds: [questionEmbed] });
+      const m = await author.send({
+        embeds: [questionEmbed]
+      });
       await m.channel
-      .awaitMessages({
-        filter: filter,
-        time: 5 * 60000,
-        max: 1,
-        errors: ['time'],
-      })
+        .awaitMessages({
+          filter: filter,
+          time: 5 * 60000,
+          max: 1,
+          errors: ['time'],
+        })
         .then((resp) => {
           if (resp.first().content.toLowerCase() === 'cancel') {
-            author.send({ embeds: [cancel] });
+            author.send({
+              embeds: [cancel]
+            });
             exitFlag = true;
           }
           if (responses.join('').length + resp.first().content.length > 4096) {
-            author.send({ embeds: [tomanychars] });
+            author.send({
+              embeds: [tomanychars]
+            });
             exitFlag = true;
           }
           responses.push(resp.first());
         })
         .catch((collected) => {
           if (collected.length) return;
-          message.channel.send({ embeds: [outoftime] });
+          message.channel.send({
+            embeds: [outoftime]
+          });
           exitFlag = true;
         });
       if (exitFlag) return;
     }
     if (exitFlag) return;
-    message.author.send(({ embeds: [success] })).then(() => {
+    message.author.send(({
+      embeds: [success]
+    })).then(async () => {
       const dataEmbed = new Discord.MessageEmbed().setTitle(`Application Submitted by ${message.author.tag}`);
       body = '';
       for (i = 0; i < responses.length; i++) {
-        body += `\n**${questions[i]}**\n${responses[i]}\n`;
+        body += `\n**${questions[i]}**${responses[i]}\n`;
       }
       body = body.trim(); // remove extra whitespace (extra \n at the end)
       dataEmbed.setDescription(body)
       const buttonsRow = new Discord.MessageActionRow()
-            .addComponents(
-                new Discord.MessageSelectMenu()
-                    .setCustomId('select')
-                    .setPlaceholder('Please Accept or Decline')
-                    .addOptions([
-                        {
-                            label: 'Accept',
-                            description: 'Accept the application',
-                            value: 'first_option',
-                        },
-                        {
-                            label: 'Decline',
-                            description: 'Decline the application',
-                            value: 'second_option',
-                        },
-                    ]),
-            );
-      message.client.channels.cache.get(staffapplicationChannel).send({embeds: [dataEmbed], components:[buttonsRow]})});
-    if (!args.length) return;
-    if (data.args[0] === 'accept') {
-      if (!message.member.roles.cache.has(staffmanagerrole)) {
-        return message.channel.send("you dont' have permission to use this command");
+        .addComponents(
+          new Discord.MessageButton()
+          .setCustomId('accept')
+          .setLabel('Accept')
+          .setStyle('SUCCESS'),
+          new Discord.MessageButton()
+          .setCustomId('decline')
+          .setLabel('Decline')
+          .setStyle('DANGER'),
+        );
+      message.client.channels.cache.get(staffapplicationChannel).send({
+        embeds: [dataEmbed],
+        components: [buttonsRow]
+      })
+      if (buttonsRow.customId === 'accept') {
+        await buttonsRow.deferUpdate();
+        await wait(4000);
+        await buttonsRow.editReply({
+          content: 'You accepted the user',
+          components: []
+        });
       }
-      const User = message.mentions.users.first();
-      if (!User) return message.channel.send('Please provide a user for me to accept');
-      User.send(`:tada: Your application to ${message.guild.name} got accepted!`);
-    }
-    if (!args.length) return;
-    if (data.args[0] === 'decline') {
-      if (!message.member.roles.cache.has(staffmanagerrole)) {
-        return message.channel.send("you dont have permission to use this command");
-      }
-      const User = message.mentions.users.first();
-      if (!User) return message.channel.send('Please provide a user for me to decline');
-      User.send(`Your application to ${message.guild.name} got declined...`);
-    }
+    });
   },
 };
